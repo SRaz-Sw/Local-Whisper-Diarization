f58e1bfc9109ed909f5702ad701ab34a9b6ddde2
DonRaz <131517474+SRaz-Sw@users.noreply.github.com>
2025-10-27 11:10:44 +0200

    Fixed layout - max-w and max-h

--- nextjs-v1/src/app/LayoutWrapper.tsx
+++ nextjs-v1/src/app/LayoutWrapper.tsx
@@ -18,7 +18,7 @@ export function LayoutWrapper({
     <SidebarProvider>
       <div className="relative w-full">
         <HomeNavbar />
-        <div className="mx-auto flex h-full max-h-[calc(100svw-4rem)] max-w-svh pt-[4rem]">
+        <div className="mx-auto flex h-full max-h-[calc(100svh-4rem)] w-full pt-[4rem]">
           {showSidebar && <HomeSidebar />}
           <main className="w-full">{children}</main>
           <Toaster position="top-center" />
